6391d614e9cafd621d21358f3eefdd9cebf6286d
Huzaifa6290 <huzaifamasood9@gmail.com>
2023-05-30 19:53:31 +0500

    2nd playround function is added for player/computer round.

--- script.js
+++ script.js
@@ -7,11 +7,37 @@
    // Use the random number to generate computer choice 
    switch (randomNumber) {
      case 0 :
-        return "Rock";
+        return "rock";
      case 1 : 
-        return "Paper";
+        return "paper";
      case 2 : 
-        return " Scissor";
+        return "scissor";
   }}
 
- console.log ( getComputerChoice() )
+ //console.log ( getComputerChoice() )
+
+
+ // Creating the 2nd playround function where a game takes place
+
+  function  playRound (playerSelection, computerSelection) 
+       {  // converts both selections to lower case for Case Insensitive Comparison 
+             playerSelection =playerSelection.toLowerCase() ;
+             computerSelection = computerSelection.toLowerCase() ;            
+            
+            //CHECK FOR A TIE
+           if ( playerSelection === computerSelection ) {   return " Its a Tie :( "      }
+            
+            // CHECK FOR PLAYER WIN CONDITIONS
+             else if (   (playerSelection==="rock" && computerSelection==="scissor")    ||  
+                        (playerSelection==="paper" && computerSelection==="rock")      ||
+                        (playerSelection==="scissor" && computerSelection==="paper")      )  
+                      { return  `You win !!! ${playerSelection} BEATS ${computerSelection}`}
+
+           // CHECK FOR COMPUTER WIN
+             else {    return ` YOU LOSE :( !!  ${computerSelection} beats ${playerSelection} `} }                                         
+ 
+
+            const playerSelection = "rock";
+            const computerSelection = getComputerChoice();
+            const result = playRound(playerSelection, computerSelection);
+            console.log(result);
